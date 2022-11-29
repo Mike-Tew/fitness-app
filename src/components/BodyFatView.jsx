be0260ca4f53ff45960b3cc04a5363513adf68ba
Michael Tew <mike.tew@gmail.com>
2022-11-29 01:08:11 -0800

    WIP: Body fat implementation.

--- src/components/BodyFatView.jsx
+++ src/components/BodyFatView.jsx
@@ -1,7 +1,21 @@
 import { Paper, TextField, Typography } from '@mui/material'
 import { Box } from '@mui/system'
+import { useEffect, useState } from 'react'
+import getBodyFatPercentage from './getBodyFat'
 
 const BodyFatView = () => {
+  const [age, setAge] = useState(0)
+  const [weight, setWeight] = useState(0)
+  const [chestSkin, setChestSkin] = useState(0)
+  const [abSkin, setAbSkin] = useState(0)
+  const [thighSkin, setThighSkin] = useState(0)
+  const [bodyFat, setBodyFat] = useState(0)
+
+  useEffect(() => {
+    let tttt = getBodyFatPercentage()
+    console.log(tttt);
+  })
+
   return (
     <>
       <Paper elevation={10}>
@@ -11,31 +25,41 @@ const BodyFatView = () => {
           </Typography>
           <Box sx={{ display: 'flex' }}>
             <Box>
-              <Typography variant="h6" gutterBottom>AGE</Typography>
+              <Typography variant="h6" gutterBottom>
+                AGE
+              </Typography>
               <TextField
                 id="outlined-basic"
                 label="years"
                 variant="outlined"
               ></TextField>
-              <Typography variant="h6" gutterBottom>WEIGHT</Typography>
+              <Typography variant="h6" gutterBottom>
+                WEIGHT
+              </Typography>
               <TextField
                 id="outlined-basic"
                 label="pounds"
                 variant="outlined"
               ></TextField>
-              <Typography variant="h6" gutterBottom>CHEST</Typography>
+              <Typography variant="h6" gutterBottom>
+                CHEST
+              </Typography>
               <TextField
                 id="outlined-basic"
                 label="mm"
                 variant="outlined"
               ></TextField>
-              <Typography variant="h6" gutterBottom>ABDOMINAL</Typography>
+              <Typography variant="h6" gutterBottom>
+                ABDOMINAL
+              </Typography>
               <TextField
                 id="outlined-basic"
                 label="mm"
                 variant="outlined"
               ></TextField>
-              <Typography variant="h6" gutterBottom>THIGH</Typography>
+              <Typography variant="h6" gutterBottom>
+                THIGH
+              </Typography>
               <TextField
                 id="outlined-basic"
                 label="mm"
@@ -43,12 +67,24 @@ const BodyFatView = () => {
               ></TextField>
             </Box>
             <Box sx={{ p: 5 }}>
-              <Typography align="center" variant="h5">Body Fat</Typography>
-              <Typography align="center" variant="h6" gutterBottom>--.-%</Typography>
-              <Typography align="center" variant="h5">Lean Mass</Typography>
-              <Typography align="center" variant="h6" gutterBottom>-- lb</Typography>
-              <Typography align="center" variant="h5">Fat Mass</Typography>
-              <Typography align="center" variant="h6">-- lb</Typography>
+              <Typography align="center" variant="h5">
+                Body Fat
+              </Typography>
+              <Typography align="center" variant="h6" gutterBottom>
+                --.-%
+              </Typography>
+              <Typography align="center" variant="h5">
+                Lean Mass
+              </Typography>
+              <Typography align="center" variant="h6" gutterBottom>
+                -- lb
+              </Typography>
+              <Typography align="center" variant="h5">
+                Fat Mass
+              </Typography>
+              <Typography align="center" variant="h6">
+                -- lb
+              </Typography>
             </Box>
           </Box>
         </Box>
---
+++ src/components/getBodyFat.js
@@ -0,0 +1,18 @@
+const getBodyFatPercentage = (
+  age = 0,
+  weight = 0,
+  chestSkin = 0,
+  abSkin = 0,
+  thighSkin = 0
+) => {
+  const fatTotal = chestSkin + abSkin + thighSkin
+  const fatSquared = Math.pow(fatTotal, 2)
+  const density =
+    1.10938 - 0.0008267 * fatTotal + 0.0000016 * fatSquared - 0.0002574 * age
+  const bodyFat = (4.57 / density - 4.142) * 100
+  const fatMass = (weight * bodyFat * 0.01).toFixed(1)
+
+  return bodyFat.toFixed(1), fatMass, weight - fatMass
+}
+
+export default getBodyFatPercentage
