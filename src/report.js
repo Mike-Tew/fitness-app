b730e6142a38eb38a99c7cb3b05aa044d5c8b22c
Michael Tew <mike.tew@gmail.com>
2023-06-12 07:27:44 -0700

    Code cleanup.

--- src/report.js
+++ src/report.js
@@ -1,29 +1,48 @@
-import { Chart } from "chart.js/auto";
+import Database from 'tauri-plugin-sql-api'
 
-(async function() {
-  const data = [
-    { year: 2010, count: 10 },
-    { year: 2011, count: 20 },
-    { year: 2012, count: 15 },
-    { year: 2013, count: 25 },
-    { year: 2014, count: 22 },
-    { year: 2015, count: 30 },
-    { year: 2016, count: 28 },
-  ];
+const db = await Database.load('sqlite:test.db')
 
-  new Chart(
-    document.getElementById('canvas'),
-    {
-      type: 'bar',
-      data: {
-        labels: data.map(row => row.year),
-        datasets: [
-          {
-            label: 'Acquisitions by year',
-            data: data.map(row => row.count)
-          }
-        ]
-      }
-    }
-  );
-})();
+await db.execute(`CREATE TABLE IF NOT EXISTS weight (
+  id INTEGER NOT NULL UNIQUE,
+  weight REAL NOT NULL,
+  date TEXT NOT NULL,
+  PRIMARY KEY(id AUTOINCREMENT))
+  `)
+
+const submitWeight = async () => {
+  const weight = document.getElementById('weight').value
+
+  await db.execute('INSERT INTO weight(weight, date) VALUES($1, DATE());', [
+    weight
+  ])
+
+  console.log(await db.select('SELECT * from weight;'))
+}
+
+document.getElementById('weight-button').addEventListener('click', submitWeight)
+
+// import { Chart } from 'chart.js/auto';
+// (async function () {
+//   const data = [
+//     { year: 2010, count: 10 },
+//     { year: 2011, count: 20 },
+//     { year: 2012, count: 15 },
+//     { year: 2013, count: 25 },
+//     { year: 2014, count: 22 },
+//     { year: 2015, count: 30 },
+//     { year: 2016, count: 28 }
+//   ]
+
+//   new Chart(document.getElementById('canvas'), {
+//     type: 'bar',
+//     data: {
+//       labels: data.map((row) => row.year),
+//       datasets: [
+//         {
+//           label: 'Acquisitions by year',
+//           data: data.map((row) => row.count)
+//         }
+//       ]
+//     }
+//   })
+// })()
